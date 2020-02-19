06566d3f4087e1b9e6c5495c6c5af8c3a62584bf
Gustavo Morini <gustavommh@gmail.com>
2020-02-19 18:40:19 -0300

    statusBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 
+import { StatusBar } from 'react-native';
+
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
