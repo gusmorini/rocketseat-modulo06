8490f2f4de83f575d29b92872b27b6ef33ecfe7a
Gustavo Morini <gustavommh@gmail.com>
2020-02-19 18:47:08 -0300

    styled-components, Container

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+/*
+
+  no react native não existe encadeamento, por exempo
+
+  View {
+
+    Text {
+
+    }
+
+  }
+
+*/
